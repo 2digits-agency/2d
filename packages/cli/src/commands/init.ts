0c3f3b0642800ef521b3b7469926651cf1e4c9f5
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-03-23 09:54:13 +0100

    ✨ Added simple git clone test

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,8 +1,46 @@
+import fs from 'fs-extra';
+import os from 'node:os';
+import pathe from 'pathe';
+import simpleGit from 'simple-git';
+
 import { createCommand } from '../helpers';
+
+async function cloneRepo(dirInRepo: string, path: string): Promise<void> {
+  const tmpDir = await fs.mkdtemp(pathe.join(os.tmpdir(), 'myapp-'));
+  console.log('Cloning repo to', tmpDir);
+  const git = simpleGit(tmpDir);
+  await git.clone('git@github.com:2digits-agency/2d-template.git', '.', ['--depth=1']);
+
+  const headCommit = await git.revparse(['HEAD']);
+  const tree = await git.raw(['ls-tree', '-r', '--name-only', `${headCommit}:${dirInRepo}`]);
+
+  const entries = tree.split('\n').filter(Boolean);
+  for (const entryPath of entries) {
+    const destPath = pathe.join(path, entryPath);
+
+    const gitObject = `${headCommit}:${dirInRepo}/${entryPath}`;
+
+    console.log(gitObject);
+
+    const type = await git.catFile(['-t', gitObject]);
+
+    if (type.trim() === 'blob') {
+      const content = await git.show([gitObject]);
+      console.log('Writing file', destPath);
+      await fs.outputFile(destPath, content);
+    }
+  }
+
+  await fs.remove(tmpDir);
+}
 
 export const init = createCommand(['init [path]', 'i'], {
   describe: 'Initialize a new project',
-  builder(args) {
+  async builder(args) {
+    const files = (await fetch('https://api.github.com/repos/V1RE/propr/contents').then((data) =>
+      data.json(),
+    )) as { name: string }[];
+
     return args
       .positional('path', {
         type: 'string',
@@ -10,7 +48,7 @@ export const init = createCommand(['init [path]', 'i'], {
         normalize: true,
       })
       .option('module', {
-        choices: ['trpc', 'stitches'] as const,
+        choices: files.map((f) => f.name),
         describe: 'Modules to install',
         type: 'array',
         alias: 'm',
@@ -23,7 +61,9 @@ export const init = createCommand(['init [path]', 'i'], {
       })
       .strict();
   },
-  handler(args) {
+  async handler(args) {
+    const path = args.path ?? process.cwd();
+    await cloneRepo('packages/ui', path);
     console.log(args.module);
   },
 });
