66edd77e8ec2a84f44372bcc21de241fb6e84531
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-03-27 01:32:48 +0200

    🚧 WIP

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,37 +1,62 @@
+import * as p from '@clack/prompts';
+import chalk from 'chalk';
 import fs from 'fs-extra';
-import os from 'node:os';
-import pathe from 'pathe';
-import simpleGit from 'simple-git';
+import path from 'pathe';
+import type { Argv } from 'yargs';
+import { z } from 'zod';
 
 import { createCommand } from '../helpers';
 
-async function cloneRepo(dirInRepo: string, path: string): Promise<void> {
-  const tmpDir = await fs.mkdtemp(pathe.join(os.tmpdir(), 'myapp-'));
-  console.log('Cloning repo to', tmpDir);
-  const git = simpleGit(tmpDir);
-  await git.clone('git@github.com:2digits-agency/2d-template.git', '.', ['--depth=1']);
+interface InitArguments {
+  name: string;
+  path: string;
+}
 
-  const headCommit = await git.revparse(['HEAD']);
-  const tree = await git.raw(['ls-tree', '-r', '--name-only', `${headCommit}:${dirInRepo}`]);
+const appName = z.string().regex(/^(?:@[\d*a-z~-][\d*._a-z~-]*\/)?[\da-z~-][\d._a-z~-]*$/, {
+  message: 'Please check https://docs.npmjs.com/cli/v9/configuring-npm/package-json#name',
+});
 
-  const entries = tree.split('\n').filter(Boolean);
-  for (const entryPath of entries) {
-    const destPath = pathe.join(path, entryPath);
+const appPath = z.string().nonempty().refine(isEmpty, 'Directory has to be empty');
 
-    const gitObject = `${headCommit}:${dirInRepo}/${entryPath}`;
+// Some existing files and directories can be safely ignored when checking if a directory is a valid project directory.
+// https://github.com/facebook/create-react-app/blob/d960b9e38c062584ff6cfb1a70e1512509a966e7/packages/create-react-app/createReactApp.js#L907-L934
+const VALID_PROJECT_DIRECTORY_SAFE_LIST = [
+  /^\.DS_Store/,
+  /^\.git/,
+  /^\.gitkeep/,
+  /^\.gitattributes/,
+  /^\.gitignore/,
+  /^\.npmignore/,
+  /^LICENSE/,
+  /^Thumbs\.db/,
+];
 
-    console.log(gitObject);
-
-    const type = await git.catFile(['-t', gitObject]);
-
-    if (type.trim() === 'blob') {
-      const content = await git.show([gitObject]);
-      console.log('Writing file', destPath);
-      await fs.outputFile(destPath, content);
-    }
+function isEmpty(dirPath: string) {
+  if (!fs.existsSync(dirPath)) {
+    return true;
   }
 
-  await fs.remove(tmpDir);
+  const files = fs.readdirSync(dirPath);
+
+  return !files.some(
+    (content) =>
+      !VALID_PROJECT_DIRECTORY_SAFE_LIST.some((safeContent) => safeContent.test(content)),
+  );
+}
+
+// const initDefaults: InitArguments = {
+//   name: 'my-2d-app',
+//   path: '.',
+// };
+
+function checkCancel<TValue>(value: TValue): Exclude<TValue, symbol> {
+  if (p.isCancel(value)) onCancel();
+
+  return value as Exclude<TValue, symbol>;
+}
+function onCancel() {
+  p.cancel('Operation cancelled.');
+  process.exit(0);
 }
 
 export const init = createCommand(['init [path]', 'i'], {
@@ -55,11 +80,45 @@ export const init = createCommand(['init [path]', 'i'], {
         type: 'boolean',
         describe: 'Install dependencies after creating the project',
       })
-      .strict();
+      .option('name', {
+        type: 'string',
+        describe: 'Name of the project',
+        alias: 'n',
+      })
+      .strict() satisfies Argv<Partial<InitArguments>>;
   },
   async handler(args) {
-    const path = args.path ?? process.cwd();
-    await cloneRepo('packages/ui', path);
-    console.log(args.module);
+    const initArguments = args;
+
+    p.intro(chalk.bgHex('#762BFF').hex('#FFFFFF').bold(' 2d init '));
+
+    p.log.info(JSON.stringify(args, undefined, 2));
+
+    initArguments.name ??= await p
+      .text({
+        message: 'What is the name of the project',
+        defaultValue: 'asdf',
+        validate(value) {
+          const result = appName.safeParse(value);
+          if (!result.success) return result.error.flatten().formErrors.join('\n');
+          return;
+        },
+      })
+      .then(checkCancel);
+
+    initArguments.path ??= await p
+      .text({
+        message: 'Where to create the project',
+        validate(value) {
+          const result = appPath.safeParse(value);
+          if (!result.success) return result.error.issues.pop()?.message;
+          return;
+        },
+      })
+      .then(checkCancel)
+      .then(path.resolve);
+
+    p.log.info(`Creating ${chalk.bold(initArguments.name)} in ${chalk.bold(initArguments.path)}`);
+    p.log.message('Installing dependencies...');
   },
 });
