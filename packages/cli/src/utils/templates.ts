4bb5c1eb59ea9e491918a14c793e6a77da1355a9
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-04-20 10:10:30 +0200

    ✨ Working on templating

--- packages/cli/src/utils/templates.ts
+++ packages/cli/src/utils/templates.ts
@@ -1,6 +1,8 @@
-import p from '@clack/prompts';
 import chalk from 'chalk';
+import { consola } from 'consola';
 import fs from 'fs-extra';
+import ignore from 'ignore';
+import ora from 'ora';
 import pathe from 'pathe';
 
 import base from '../../templates/base/package.json';
@@ -15,18 +17,40 @@ export type Template = keyof typeof templates;
 
 export async function copyTemplate(template: Template, path: string) {
   const sourceDir = pathe.join(PKG_ROOT, 'templates', template);
-  const spinner = p.spinner();
 
-  spinner.start(`Copying ${chalk.bold(`${template} template`)} to ${chalk.bold(path)}...`);
+  const ignoreFile = pathe.join(sourceDir, '.gitignore');
+
+  const ig = ignore();
+
+  if (await fs.exists(ignoreFile)) {
+    const ignoreContents = await fs.readFile(ignoreFile, { encoding: 'utf8' });
+
+    ig.add(ignoreContents.split('\n').filter((line) => ignore.isPathValid(line)));
+  }
+
+  const spinner = ora({
+    text: `Copying ${chalk.bold(`${template} template`)} to ${chalk.bold(path)}...`,
+  }).start();
 
   try {
-    await fs.copy(sourceDir, path);
+    await fs.copy(sourceDir, path, {
+      filter(src, dest) {
+        const relative = pathe.relative(sourceDir, src);
 
-    spinner.stop(
+        consola.debug('src: ' + src, 'dest: ' + dest);
+
+        spinner.suffixText = chalk.dim(relative);
+
+        // return !(relative && ig.ignores(relative));
+        return true;
+      },
+    });
+
+    spinner.succeed(
       chalk.green(`Copied ${chalk.bold(`${template} template`)} to ${chalk.bold(path)}`),
     );
   } catch {
-    spinner.stop(
+    spinner.fail(
       chalk.red(`Failed to copy ${chalk.bold(`${template} template`)} to ${chalk.bold(path)}`),
     );
 
