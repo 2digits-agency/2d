027e8b34f7a7d9c936223a0418135a3af040cbdb
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-04-21 11:18:09 +0200

    🔊 Added debug logging

--- packages/cli/src/utils/dependencies.ts
+++ packages/cli/src/utils/dependencies.ts
@@ -1,7 +1,10 @@
 import chalk from 'chalk';
+import { createConsola } from 'consola';
 import { execa } from 'execa';
 
 import { Spinner } from './log';
+
+const consola = createConsola({ defaults: { tag: 'utils/dependencies' } });
 
 export async function installDependencies(path: string) {
   const spinner = new Spinner();
@@ -9,10 +12,16 @@ export async function installDependencies(path: string) {
   spinner.start(`Installing dependencies...`);
 
   try {
-    await execa('pnpm', ['install'], { cwd: path });
+    const { command, stdout, stderr } = await execa('pnpm', ['install'], { cwd: path });
+
+    consola.debug('command:', command);
+    consola.debug('stdout:', stdout);
+    consola.debug('stderr:', stderr);
 
     spinner.success(`Installed dependencies`);
-  } catch {
+  } catch (error) {
+    consola.fatal(error);
+
     spinner.fail(chalk.red(`Failed to install dependencies`));
   }
 }
