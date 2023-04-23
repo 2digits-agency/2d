1994118eb681d27a913df1c8784cf82ccbc89dbe
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-04-23 19:50:32 +0200

    ♻️ Refactored rename.ts

--- packages/cli/src/utils/rename.ts
+++ packages/cli/src/utils/rename.ts
@@ -1,17 +1,38 @@
 import { consola } from 'consola';
 import fs from 'fs-extra';
-import { globbyStream } from 'globby';
+import { globby } from 'globby';
 
-export async function renamePlaceholders(path: string) {
-  const placeholders = globbyStream(['**/_2d_**'], {
+export async function findPlaceholders(path: string): Promise<string[]> {
+  const placeholders = await globby(['**/_2d_**'], {
     cwd: path,
     absolute: true,
+    dot: true,
   });
 
-  for await (const placeholder of placeholders) {
-    const renamed = placeholder.toString().replace(/_2d_/g, '');
-    consola.debug(placeholder, renamed);
+  consola.debug(`Found ${placeholders.length} placeholders:`);
+  consola.debug(placeholders);
 
-    await fs.rename(placeholder, renamed);
+  return placeholders;
+}
+
+export function renameFile(placeholderPath: string): Promise<void> {
+  const newPath = placeholderPath.replace(/_2d_/g, '');
+
+  const renamed = fs.rename(placeholderPath, newPath);
+
+  consola.debug(`Renamed ${placeholderPath} to ${newPath}`);
+
+  return renamed;
+}
+
+export async function renamePlaceholders(path: string): Promise<void> {
+  const placeholderPaths = await findPlaceholders(path);
+
+  const renamePromises = [];
+
+  for (const placeholderPath of placeholderPaths) {
+    renamePromises.push(renameFile(placeholderPath));
   }
+
+  await Promise.all(renamePromises);
 }
