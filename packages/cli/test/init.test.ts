cb25c9412abbd942ab878060b7be48dbb2c4f490
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-04-28 15:00:52 +0200

    📸 Updated snapshots

--- packages/cli/test/init.test.ts
+++ packages/cli/test/init.test.ts
@@ -1,26 +1,27 @@
-import { consola } from 'consola';
 import fs from 'fs-extra';
 import { globby } from 'globby';
 import mock from 'mock-fs';
+import pathe from 'pathe';
 import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 
 import { init } from '../src/commands/init';
 import { TEMPLATE_DIR } from '../src/constants';
 
 describe('init', () => {
-  beforeEach(() => {
-    mock({
-      [TEMPLATE_DIR]: mock.load(TEMPLATE_DIR),
-    });
+  beforeEach(async () => {
+    const files = await globby('./**/*', { dot: true, gitignore: true, cwd: TEMPLATE_DIR });
 
-    consola.wrapAll();
-    consola.pauseLogs();
+    const mockFiles = {} as Record<string, string>;
+
+    for (const file of files) {
+      const path = pathe.join(TEMPLATE_DIR, file);
+      mockFiles[path] = await fs.readFile(path, { encoding: 'utf8' });
+    }
+
+    mock(mockFiles);
   });
 
   afterEach(() => {
-    consola.resumeLogs();
-    consola.restoreAll();
-
     mock.restore();
   });
 
