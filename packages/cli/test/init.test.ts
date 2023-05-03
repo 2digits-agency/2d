6c517886eb93f08db7b45bd6b7dcc2e7bb8e6a83
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-05-03 16:55:18 +0200

    🧪 Added stitches init test

--- packages/cli/test/init.test.ts
+++ packages/cli/test/init.test.ts
@@ -33,6 +33,19 @@ describe('base', () => {
     await snapshotCliOutputFs();
   });
 
+  describe('stitches', () => {
+    it('should add a stitches module', async () => {
+      await init.handler({
+        path: './test/path',
+        module: ['stitches'],
+        install: false,
+        name: 'test',
+      });
+
+      await snapshotCliOutputFs();
+    });
+  });
+
   describe('web', () => {
     it('should create a web app', async () => {
       await init.handler({
