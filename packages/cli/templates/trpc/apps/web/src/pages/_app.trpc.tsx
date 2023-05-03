9824928b2a39e599e957f823fb3a11da0bfbe397
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-05-03 12:16:51 +0200

    ♻️ Refactored trpc web _app to use patch

--- packages/cli/templates/trpc/apps/web/src/pages/_app.trpc.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-
-import { trpc } from '../utils/trpc';
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default trpc.withTRPC(App);
