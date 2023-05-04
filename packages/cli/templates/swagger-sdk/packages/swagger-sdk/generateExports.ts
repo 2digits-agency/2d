04f2e5d292956e2d3928406af2b345c6839f89fd
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-05-04 10:32:05 +0200

    ✨ Added initial swagger-sdk module

---
+++ packages/cli/templates/swagger-sdk/packages/swagger-sdk/generateExports.ts
@@ -0,0 +1,84 @@
+import { genExport } from 'knitwork';
+import { readdir, writeFile } from 'node:fs/promises';
+import path from 'pathe';
+import Prettier from 'prettier';
+import consola from 'consola';
+
+const logger = consola.withTag('orval');
+
+logger.wrapAll();
+
+export async function generateExports() {
+  const exports: string[] = [];
+
+  const src = path.normalize('./src');
+
+  logger.info('Generating exports in', src);
+
+  const contents = await readdir(src, { withFileTypes: true });
+
+  logger.withTag('contents').debug(contents);
+
+  const entries = contents.filter((item) => item.isDirectory());
+
+  logger.withTag('entries').debug(entries);
+
+  for (const entry of entries) {
+    const entryLogger = logger.withTag('entries').withTag(entry.name);
+    entryLogger.info('Generating exports for', entry.name);
+
+    const entryPath = path.join(src, entry.name);
+
+    const entryFiles = await readdir(entryPath, { withFileTypes: true });
+
+    entryLogger.debug(entryFiles);
+
+    for (const file of entryFiles) {
+      const exportLogger = entryLogger.withTag(file.name);
+
+      exportLogger.debug('Checking', file.name);
+
+      if (file.isFile()) {
+        const extension = path.extname(file.name);
+        exportLogger.debug('Extension:', extension);
+
+        const baseName = path.basename(file.name, extension);
+        exportLogger.debug('Base name:', baseName);
+
+        const filePath = `./${path.relative(src, path.join(entryPath, baseName))}`;
+        exportLogger.debug('Relative path:', filePath);
+
+        const exportStatement = genExport(filePath, { name: '*' });
+        exportLogger.debug('Generated export:', exportStatement);
+
+        exports.push(exportStatement);
+      }
+    }
+
+    exports.push('\n');
+  }
+
+  logger.debug('Generated exports:', exports);
+
+  const filepath = path.join(src, 'index.ts');
+
+  try {
+    const fileContents = Prettier.format(exports.join('\n'), {
+      filepath,
+      parser: 'typescript',
+      singleQuote: true,
+    });
+
+    await writeFile(filepath, fileContents);
+
+    logger.success('Wrote exports to', filepath);
+  } catch (error) {
+    logger.error('Failed to write exports to', filepath);
+    if (error instanceof Error) {
+      logger.fatal(error);
+    }
+
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  }
+}
---
+++ packages/cli/templates/swagger-sdk/packages/swagger-sdk/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+import { name } from './package.json';
+
+export default defineConfig({
+  minify: true,
+  target: 'esnext',
+  skipNodeModulesBundle: true,
+  sourcemap: true,
+  treeshake: true,
+  format: ['esm', 'cjs'],
+  entry: ['src/index.ts', 'src/*/*.ts'],
+  clean: true,
+  dts: true,
+  name,
+});
