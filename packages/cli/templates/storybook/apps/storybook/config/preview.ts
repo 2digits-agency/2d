eefdc3183dca5ac89345cf61346b5f634d5a4da0
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-05-04 14:49:21 +0200

    ✨ Added basic storybook template

---
+++ packages/cli/templates/storybook/apps/storybook/config/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from '@storybook/react';
+
+import '@mod/stitches/reset';
+
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+} satisfies Preview;
+
+export default preview;
