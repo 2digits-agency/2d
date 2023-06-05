907e2b94900177d3c405e52f698046d6a8c7c211
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-06-05 13:36:12 +0200

    ✨ Added tailwind module to code

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -7,6 +7,7 @@ import gqlCodegen from '../templates/gql-codegen/packages/gql-codegen/package.js
 import module from '../templates/module/package.json';
 import storybook from '../templates/storybook/apps/storybook/package.json';
 import swaggerSdk from '../templates/swagger-sdk/packages/swagger-sdk/package.json';
+import tailwind from '../templates/tailwind/packages/tailwind/package.json';
 import trpc from '../templates/trpc/packages/trpc/package.json';
 import web from '../templates/web/apps/web/package.json';
 
@@ -23,6 +24,7 @@ export const TEMPLATE_DIR = pathe.join(PKG_ROOT, 'templates');
 export const templates = {
   base,
   web,
+  tailwind,
   trpc,
   ['swagger-sdk']: swaggerSdk,
   storybook,
