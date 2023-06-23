8b6aef325db020c5bba6ec8f71de75635aa70da8
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-06-23 15:33:26 +0200

    🔧 Updated eslint turbo rules

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'tsup';
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars
 const isDev = process.env.npm_lifecycle_event === 'dev';
 
 export default defineConfig({
